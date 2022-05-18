570d1c1042704af9553c1cf58052a0419d96105b
zmprunella <zane.prunella@gmail.com>
2022-05-18 09:48:57 -0700

    As an unregistered user, I can navigate to a page that shows me a list of all the apartments available for rent.

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -5,7 +5,14 @@ export default class ApartmentIndex extends Component {
   render() {
     return (
       <>
-      <h3>ApartmentIndex</h3>
+      <h3>Apartments Available</h3>
+        <ul>
+          <li>North Park</li>
+          <li>South Park</li>
+          <li>Bankers Hill</li>
+          <li>Pacific Beach</li>
+          <li>Ocean Beach</li>
+        </ul>
       </>
     )
   }
