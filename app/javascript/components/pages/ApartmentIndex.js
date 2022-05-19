339c347abd8b9c4f662e59264eebb7efd555cdfd
zmprunella <zane.prunella@gmail.com>
2022-05-19 11:31:52 -0700

    rendered list dynamically

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react'
+import { Card, CardImg, CardTitle, Col } from 'reactstrap'
+
 
 
 export default class ApartmentIndex extends Component {
   render() {
     return (
-      <>
-      <h3>Apartments Available</h3>
-        <ul>
-          <li>North Park</li>
-          <li>South Park</li>
-          <li>Bankers Hill</li>
-          <li>Pacific Beach</li>
-          <li>Ocean Beach</li>
-        </ul>
+    <>
+        <h3>Check Out All These Beautiful Apartments!</h3>
+        <br />
+        <Col sm="6">
+          {this.props.apartments && this.props.apartments.map(apartment => {
+            return(
+              <Card body key={apartment.street}>
+                <CardImg alt='apartment image' src= {apartment.image}/>
+                <br />
+                <CardTitle>
+                  <h3>{apartment.street}</h3>
+                  <h4>{apartment.price}</h4>
+                </CardTitle>
+              </Card>
+            )
+          })}
+        </Col>
       </>
     )
   }
